994b2a69d6da25150d38e6c0ba31c5dd0eca2363
Nikhil Prabhakar U <nikhil@mapbox.com>
2016-08-09 17:09:16 +0530

    Updating the signage list

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ function init() {
     // Fetch data every 10 minutes
     refreshData(10);
 
-    var mapillaryRestrictionsFilter = ["in", "value", "regulatory--no-left-turn--us", "regulatory--no-right-turn--us", "regulatory--no-straight-through--us", "regulatory--no-u-turn--us", "regulatory--no-left-or-u-turn--us", "regulatory--no-left-turn--ca", "regulatory--no-right-turn--ca", "regulatory--no-straight-through--ca", "regulatory--no-u-turn--ca", "regulatory--no-left-or-u-turn--ca", "regulatory--no-left-turn", "regulatory--no-right-turn", "regulatory--no-straight-through", "regulatory--no-u-turn", "regulatory--no-left-or-u-turn"]
+    var mapillaryRestrictionsFilter = ["in", "value", "regulatory--no-left-turn--us", "regulatory--no-right-turn--us", "regulatory--no-straight-through--us", "regulatory--no-u-turn--us", "regulatory--no-left-or-u-turn--us", "regulatory--no-left-turn--ca", "regulatory--no-right-turn--ca", "regulatory--no-straight-through--ca", "regulatory--no-u-turn--ca", "regulatory--no-left-or-u-turn--ca", "regulatory--no-left-turn", "regulatory--no-right-turn", "regulatory--no-straight-through", "regulatory--no-u-turn", "regulatory--no-left-or-u-turn", "mandatory--turn-left--de","mandatory--proceed-straight-or-turn-left--de","mandatory--turn-right--de","mandatory--proceed-straight-or-turn-right--de","mandatory--proceed-straight--de","mandatory--turn-left-ahead--de","mandatory--turn-right-ahead--de"]
 
     var mapillaryTraffic = {
         "id": "mapillaryTraffic",
